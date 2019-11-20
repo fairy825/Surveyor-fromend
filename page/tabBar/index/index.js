81a32a4936e01a3b9dc3c51b7a4dea44f6afe2f0
fairy825 <1324240571@qq.com>
2019-11-20 14:16:23 +0800

    图标安卓兼容

--- page/tabBar/index/index.js
+++ page/tabBar/index/index.js
@@ -7,11 +7,11 @@ Page({
   data: {
     categories: [
       { icon: '/assets/index/write.png', text: '发布问卷', type: 'navigate',url: '/page/create/pages/beforeCreate/beforeCreate' },
-      { icon: '/assets/index/显示.png', text: '回答问卷', type: 'switchTab', url: '/page/tabBar/messages/messages' },
+      { icon: '/assets/index/show.png', text: '回答问卷', type: 'switchTab', url: '/page/tabBar/messages/messages' },
       { icon: '/assets/index/person.png', text: '个人中心', type: 'switchTab',url: '/page/tabBar/profile/profile' },
-      { icon: '/assets/index/钱包.png', text: '我的钱包', type: 'navigate',  url: '/page/info/pages/myPacket/myPacket'},
-      { icon: '/assets/index/数据图.png', text: '我的问卷', type: 'navigate',  url: '/page/info/pages/myQuest/myQuest'},
-      { icon: '/assets/index/文本.png', text: '我的答卷', type: 'navigate',  url: '/page/info/pages/myAnswer/myAnswer'},  
+      { icon: '/assets/index/packet.png', text: '我的钱包', type: 'navigate',  url: '/page/info/pages/myPacket/myPacket'},
+      { icon: '/assets/index/analysis.png', text: '我的问卷', type: 'navigate',  url: '/page/info/pages/myQuest/myQuest'},
+      { icon: '/assets/index/text.png', text: '我的答卷', type: 'navigate',  url: '/page/info/pages/myAnswer/myAnswer'},  
     ],
     imgUrls: [
       '/assets/banner1.jpg',
